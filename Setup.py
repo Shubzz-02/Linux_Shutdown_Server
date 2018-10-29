0687ded8639895bda966d10960eb621ef77c2c46
Subhash Rawat <19619948+Shubzz-02@users.noreply.github.com>
2018-10-29 14:52:22 +0000

    completed Setup.py

--- Setup.py
+++ Setup.py
@@ -1,25 +1,59 @@
 #!/usr/bin/env python
 import os
+from os import path
+import shutil
+import subprocess
 
 
 def start():
     if os.geteuid() is not 0:
         print "[-] Root permission required"
-        print "[+] Run command chmod +x ./Setup"
-        print "[+] sudo ./Setup "
+        print "[+] Run command chmod +x ./Setup.py"
+        print "[+] sudo ./Setup.py "
+        return False
+    else:
+    	return True
 
 
 def write_file():
-    os.chdir("/etc/systemd/system")
-    fo = open("shutdown_client.service","w")
-    fo.write("[Unit]\nDescription=Shutdown Client Service\nAfter=dbus.service\n"
-             "\n[Service]\nExecStart=/usr/local/bin/node.py\n"
-             "\n[Install]\nWantedBy=default.target")
+	try:
+		os.chdir("/etc/systemd/system")
+		fo = open("shutdown_client.service","w")
+		fo.write("[Unit]\nDescription=Shutdown Client Service\nAfter=dbus.service\n"
+				 "\n[Service]\nExecStart=/usr/local/bin/node.py\n"
+				 "\n[Install]\nWantedBy=default.target")
+		fo.close()
+		os.chmod("shutdown_client.service",0644)
+		return True
+	except Exception as error:
+		print error
+		return False
+		
 
 
-start()
+
+def copy_file():
+	if path.exists("node.py"):
+		shutil.copy("node.py","/usr/local/bin/node.py")
+		os.chmod("/usr/local/bin/node.py",0744)
+		return True
+	else:
+		print "[-] File node.py no found"
+		print "[-] Check if same location contains 'node.py'"
+		return False
+
+def initialize():
+	subprocess.check_output(["systemctl","daemon-reload"],stderr=subprocess.STDOUT)
+	subprocess.check_output(["systemctl","enable","shutdown_client.service"],stderr=subprocess.STDOUT)
+	
+
+if start():
+	if copy_file():
+		if write_file():
+			initialize()
+			
 
 
 
-https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/7/html/system_administrators_guide/sect-managing_services_with_systemd-unit_files
-https://linuxconfig.org/how-to-automatically-execute-shell-script-at-startup-boot-on-systemd-linux
+'https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/7/html/system_administrators_guide/sect-managing_services_with_systemd-unit_files'
+'https://linuxconfig.org/how-to-automatically-execute-shell-script-at-startup-boot-on-systemd-linux'
